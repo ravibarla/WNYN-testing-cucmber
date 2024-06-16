f535375ff9b42aa27fcc62e9e396ee5e5ddb7813
ravi barla <ravibarla1@gmail.com>
2024-06-16 20:02:22 +0530

    adding symbolAmount object

---
+++ game_config/WNYN_property.js
@@ -0,0 +1,27 @@
+property = {
+  total_win_number: 3,
+  total_your_number: 10,
+  total_symbols: 10,
+};
+
+symbolAmount = {
+  A: 10,
+  B: 20,
+  C: 50,
+  J: 100,
+  N: 500,
+  S: 1000,
+  Z: 400000,
+};
+
+amgtvAmount = {
+  AAA: 10,
+  BBB: 20,
+  CCC: 50,
+  JJJ: 100,
+  NNN: 500,
+  SSS: 1000,
+  ZZZ: 400000,
+};
+
+module.exports={property,symbolAmount}
